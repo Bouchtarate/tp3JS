60c9b836c3e67e4dd288671c312eaa59f82c1cfc
Bouchtarate <hamza.bouchtarate@gmail.com>
2024-11-16 19:36:48 +0100

    start TP3 for school

---
+++ main.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const product = { Nom, prix, description };
+
+let pannier = [product, product, product];
+
+const payment = { carte, num, date, CCV };
+
+const CheckoutForm = {
+  pannier: pannier,
+  nom,
+  prenom,
+  userName,
+  Adresse1,
+  Adresse2,
+  Ville,
+  Pays,
+  payment: payment,
+};
